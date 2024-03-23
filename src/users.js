8ec9915a01ea205b16373349640ec6dcbf3dd875
luislolih <luislolihuamanchumo@gmail.com>
2024-03-23 01:52:35 -0500

    edit users

--- src/users.js
+++ src/users.js
@@ -1,5 +1,5 @@
 const users = [
-    { email: "123", password: "123" },
+    { email: "123@correo.com", password: "123" },
     { email: "usuario2@example.com", password: "password2" },
     { email: "usuario3@example.com", password: "password3" },
 ];
